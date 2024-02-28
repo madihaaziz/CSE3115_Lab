00f5b84c9791addcddde358f13585d3fe4edd949
Madiha Aziz <126233391+madihaaziz@users.noreply.github.com>
2024-02-28 05:21:55 +0000

    update

--- 28_02_24/file4.py
+++ 28_02_24/file4.py
@@ -13,7 +13,19 @@
 #     break
 #   i+=1
 
-for i in range(1,11,1):
-  print(i)
-for i in range(0,11,2):
-  print(i)
+# for i in range(1,11,1):
+#   print(i)
+# for i in range(0,11,2):
+#   print(i)
+
+a=int(input())
+c=0
+for i in range(1,a+1):
+  if a%i==0:
+    c+=1
+  else:
+    continue
+if c==2:
+  print("Prime")
+else:
+  print("composite")
---
+++ 28_02_24/file5.py
@@ -0,0 +1,18 @@
+# for i in range(5):
+#   for j in range(4):
+#     print("*",end=" ")
+#   print()
+
+
+# for i in range(1,6):
+#   for j in range(i):
+#     print("*",end=" ")
+#   print()
+
+for i in range(1,6):
+  for j in range(5-i):
+    x=' '
+  while i!=0:
+    print(" ","*")
+    i-=1
+  print()
