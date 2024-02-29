e0191fd319932b49547083a82b8ab7ed07230e60
Madiha Aziz <126233391+madihaaziz@users.noreply.github.com>
2024-02-29 03:46:04 +0000

    patternfile

--- 28_02_24/file5.py
+++ 28_02_24/file5.py
@@ -1,18 +1,37 @@
+# Pattern 1
 # for i in range(5):
 #   for j in range(4):
 #     print("*",end=" ")
 #   print()
 
-
+#Pattern 2
 # for i in range(1,6):
 #   for j in range(i):
 #     print("*",end=" ")
 #   print()
 
-for i in range(1,6):
-  for j in range(5-i):
-    x=' '
-  while i!=0:
-    print(" ","*")
-    i-=1
-  print()
+#Pattern 3
+# for i in range(6,0,-1):
+#   for j in range(1,i):
+#     print(" ",end=" ")
+#   for k in range(6-i+1):
+#     print("*",end=" ")
+#     i-=1
+#   print()
+
+#Pattern 4
+# for i in range(6,0,-1):
+#   for j in range(1,i):
+#     print(end=" ")
+#   for k in range(6-i+1):
+#     print("*",end=" ") 
+#   print()
+
+#Pattern 5
+# for i in range(6,0,-1):
+#   for j in range(6-i+1):
+#     print(" ",end=" ")
+#   for k in range(1,i):
+#     print("*",end=" ")
+#     i-=1
+#   print()
