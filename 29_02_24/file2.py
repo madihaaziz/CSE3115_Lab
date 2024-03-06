871737b0114e4aa58f963d7d2676a3cf0eb3cc60
Madiha Aziz <126233391+madihaaziz@users.noreply.github.com>
2024-03-06 03:30:22 +0000

    commit

--- 29_02_24/file2.py
+++ 29_02_24/file2.py
@@ -1,19 +1,17 @@
-ul=int(input())
-ll=int(input())
-sum=0
-cn=0
-for i in range(ll,ul+1,1):
+ul = int(input())
+ll = int(input())
+sum = 0
+cn = 0
+for i in range(ll, ul + 1, 1):
   # print(i)
-  c=0
-  for j in range(1,i+1):
-    if i%j==0:
-      c+=1
-  if c==2:
-    sum+=i
-    cn+=1
-    print(i,end=" ")
+  c = 0
+  for j in range(1, i + 1):
+    if i % j == 0:
+      c += 1
+  if c == 2:
+    sum += i
+    cn += 1
+    print(i, end=" ")
 print()
-print("Sum of prime numbers: ",sum)
-print("Number of prime numbers: ",cn)
-    
-  
+print("Sum of prime numbers: ", sum)
+print("Number of prime numbers: ", cn)
